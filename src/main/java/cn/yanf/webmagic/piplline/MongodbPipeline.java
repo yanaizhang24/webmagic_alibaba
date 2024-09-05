bee98f34fdef3bae8fded280f34e01b7c45548c3
yanf <yanaizhang24@yeah.net>
2024-09-05 11:39:00 +0800

    Update MongodbPipeline.java

--- src/main/java/cn/yanf/webmagic/piplline/MongodbPipeline.java
+++ src/main/java/cn/yanf/webmagic/piplline/MongodbPipeline.java
@@ -14,20 +14,49 @@ import java.util.Map;
  * Created by Administrator on 2016/9/18 0018.
  */
 public class MongodbPipeline implements Pipeline {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+      // 连接到 mongodb 服务
+      public static MongoClient mongoClient = new MongoClient(
+              new MongoClientURI("uri" ) );
+
     public MongodbPipeline() {
     }
     private List<TieBar> t;
     public void process(ResultItems resultItems, Task task) {
-        System.out.println("get page: " + resultItems.getRequest().getUrl());
-        Iterator i$ = resultItems.getAll().entrySet().iterator();
-        while(i$.hasNext()) {
-            Map.Entry entry = (Map.Entry)i$.next();
-            System.out.println((String)entry.getKey() + ":\t" + entry.getValue());
-        }
-        if(resultItems.getAll().containsKey("list_ali")){
-            SpringUtils.writeData((List<TieBar>)resultItems.getAll().get("list_ali"),"ali");
-        }
+//        System.out.println("get page: " + resultItems.getRequest().getUrl());
+//        Iterator i$ = resultItems.getAll().entrySet().iterator();
+//        while(i$.hasNext()) {
+//            Map.Entry entry = (Map.Entry)i$.next();
+//            System.out.println((String)entry.getKey() + ":\t" + entry.getValue());
+//        }
+        Map<String,Object> result=resultItems.getAll();
+        //是列表页信息
+        if(resultItems.getAll().containsKey("pageInfo")){
+//            SpringUtils.writeData((List<TieBar>)resultItems.getAll().get("list_ali"),"ali");
+            try {
+                Document document=new Document();
+                document.append("pageUrl",result.get("pageUrl"));
+                document.append("bugLists",result.get("bugLists"));
+                mongoClient.getDatabase("ku").getCollection("bug_page_info").insertOne(document);
+            } catch (Exception e) {
+                logger.error("mongodb",e);
+            }
 
+        }
+        //是详情页信息
+        if(resultItems.getAll().containsKey("bugInfo")){
+//            SpringUtils.writeData((List<TieBar>)resultItems.getAll().get("list_ali"),"ali");
+            try {
+                Document document=new Document(result);
+//                document.append("pageUrl",result.get("pageUrl"));
+//                document.append("bugLists",result.get("bugLists"));
+                mongoClient.getDatabase("ku").getCollection("bug_all_info").insertOne(document);
+            } catch (Exception e) {
+                logger.error("mongodb",e);
+            }
+
+
+        }
 
 
     }
